023e438dc002c347980b9aa12f8258f73835491e
roothex200 <sabiturrahman60@gmail.com>
2023-08-11 20:00:45 +0600

    initial commit bismilla

---
+++ android/app/src/main/kotlin/com/example/eshopy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eshopy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:eshopy/src/core/utils/colors/app_colors.dart';
+import 'package:eshopy/src/main/presentation/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child:MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'eshopy',
+      theme: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: Appcolor.appbg
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
